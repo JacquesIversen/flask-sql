a9adfc82bdaad2d3e96c48cbbc04c7cdfcc45321
JacquesIversen <jackiracki1@gmail.com>
2023-06-25 13:50:49 +0000

    base and task html updated. materizale added

---
+++ static/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initializiartion
+    var elems = document.querySelectorAll('.sidenav');
+    M.Sidenan.init(sidenav);
+  });
